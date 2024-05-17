4e00e173537b667060951a07ae82116188b9bd7c
Raphael Feitosa Assumpção <raphaelassumpcao00@gmail.com>
2024-05-17 13:52:24 -0300

    Progeto de contagem regressiva usando 'for'

---
+++ Ex011.py
@@ -0,0 +1,6 @@
+# Crie um projeto onde mostre acontagem regressiva de uma bomba
+import time
+for c in range(11,0, -1):
+    print('Contagem regressiva {}'.format(c-1))
+    time.sleep(0.5)
+print('BOMMMMMMM!!')
