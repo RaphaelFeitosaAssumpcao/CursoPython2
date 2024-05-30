723b9d32751968776acb34d01c9ce7eb30db1197
Raphael Feitosa Assumpção <raphaelassumpcao00@gmail.com>
2024-05-30 16:06:53 -0300

    programa que leia o nome e o preço de vários produtos, com funcionalidades do while

--- Ex031.py
+++ Ex031.py
@@ -1,11 +1,11 @@
 # Crie um programa que leia o nome e o preço de vários produtos. O programa deverá perguntar se o usuário vai continuar ou não
 # Qual o total gasto na compra - quantos produtos custam mais de R$1000 - qual é o nome do produto mais barato
 
-total = soma = totalp = menor = cont = 0
+total = somat = totalp = menor = cont = 0
 while True:
     nproduto = str(input('Digite o nome do produtor: '))
     total = float(input('Digite o valor do produto: R$ '))
-    soma += total
+    somat += total
     cont +=1
     if cont == 1:
         menor = total
@@ -19,6 +19,6 @@ while True:
         continuar = str(input('Quer continuar?[S/N]: ')).upper().strip()[0]
     if continuar == 'N':
         break
-print(f'O total de produtos comprados é {totalp}, o valor de tudo deu {soma} ')
+print(f'O total de produtos comprados é {totalp}, o valor de tudo deu {somat} ')
 print(f'O produto mais barato é R${menor}')
 
